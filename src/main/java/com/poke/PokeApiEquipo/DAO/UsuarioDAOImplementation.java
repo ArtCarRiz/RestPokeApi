b0ce423d0d6ebf1254418123a5c09cebbb98c3dd
digis <digis@LAPTOP-AD7LFKO5>
2026-04-23 18:16:09 -0600

    addUsuarioListo

--- src/main/java/com/poke/PokeApiEquipo/DAO/UsuarioDAOImplementation.java
+++ src/main/java/com/poke/PokeApiEquipo/DAO/UsuarioDAOImplementation.java
@@ -5,25 +5,47 @@
 package com.poke.PokeApiEquipo.DAO;
 
 import com.poke.PokeApiEquipo.ML.Result;
+import com.poke.PokeApiEquipo.ML.Rol;
 import com.poke.PokeApiEquipo.ML.Usuario;
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 /**
  *
  * @author digis
  */
+@Repository
 public class UsuarioDAOImplementation implements IUsuario{
+    
+    @Autowired
+    private EntityManager entityManager;
 
     @Override
+    @Transactional
     public Result Add(Usuario usuario) {
         Result result = new Result();
         try {
+            
+            Usuario usuarioMl = new Usuario();
+            
+            usuarioMl.setUserName(usuario.getUserName());
+            usuarioMl.setPassword(usuario.getPassword());
+            usuarioMl.setCorreo(usuario.getCorreo());
+            
+            usuarioMl.Rol = new Rol();
+            usuarioMl.Rol.setIdRol(usuario.Rol.getIdRol());
+            
+            entityManager.persist(usuarioMl);
+            result.correct = true;
             
         } catch (Exception e) {
             result.correct = false;
             result.errorMessage = e.getLocalizedMessage();
             result.ex = e;
         }
-        return null;
+        return result;
     }
     
 }
--- src/main/java/com/poke/PokeApiEquipo/RestController/PruebaRestController.java
+++ src/main/java/com/poke/PokeApiEquipo/RestController/PruebaRestController.java
@@ -1,11 +1,16 @@
 
 package com.poke.PokeApiEquipo.RestController;
 
+import com.poke.PokeApiEquipo.DAO.UsuarioDAOImplementation;
 import com.poke.PokeApiEquipo.ML.Result;
+import com.poke.PokeApiEquipo.ML.Usuario;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +19,9 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 @RequestMapping("/pokemon")
 public class PruebaRestController {
+    
+    @Autowired
+    private UsuarioDAOImplementation usuarioDAOImplementation;
     
     String rutaBase = "https://pokeapi.co/api/v2/pokemon/";
    
@@ -47,6 +55,24 @@ public class PruebaRestController {
             result.ex = e;
         }
         return ResponseEntity.badRequest().body(result);
+    }
+    
+    @PostMapping
+    public ResponseEntity addUsuario(@RequestBody Usuario usuario){
+        Result result = new Result();
+        try {
+            result = usuarioDAOImplementation.Add(usuario);
+            if (result.correct) {
+                return ResponseEntity.ok(result);
+            }else{
+                return ResponseEntity.status(400).body(result);
+            }
+        } catch (Exception e) {
+            result.correct = false;
+            result.errorMessage = e.getLocalizedMessage();
+            result.ex = e;
+        }
+        return ResponseEntity.status(500).body(result);
     }
     
 
